b8e5e8a1af4ea8c9e86fb86659b8d8074527ad56
tuancoltech <tuancoltech@gmail.com>
2025-11-17 08:40:09 +0700

    Add Preview for every compose component

--- app/src/main/java/com/crypto/demo/ui/theme/Theme.kt
+++ app/src/main/java/com/crypto/demo/ui/theme/Theme.kt
@@ -2,9 +2,11 @@ package com.crypto.demo.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -30,4 +32,12 @@ fun CurrencyDemoTheme(
         typography = Typography,
         content = content
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CurrencyDemoThemePreview() {
+    CurrencyDemoTheme {
+        Text(text = "Currency Demo Preview")
+    }
 }
